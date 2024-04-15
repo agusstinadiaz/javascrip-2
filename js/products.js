4d178325184931f72ec3fe9ecf46db3c316b0cec
Agustina Diaz <agus122804@gmail.com>
2024-04-15 19:39:01 -0300

    Entrega 2 Javascrip

---
+++ js/products.js
@@ -0,0 +1,29 @@
+const productos = [ {
+    id: 1,
+    nombre: 'Texanas Meghan',
+    precio: 73900,
+    img: "https://cdn.shopk.it/usercontent/camelia/media/images/89ecf8e-183422-img_0772.jpeg",
+    cantidad: 1,
+ }, 
+ {
+    id: 2,
+    nombre: 'Borcegos Aspen',
+    precio: 65000,
+    img: "https://acdn.mitiendanube.com/stores/001/337/723/products/96ad095b-8912-4910-b5d2-a6cfb86fc24f1-36a281cd4ec47b71f716753065952506-640-0.jpeg",
+    cantidad: 1,
+ },
+ {
+    id: 3,
+    nombre: 'Zapatillas Nala',
+    precio: 60000,
+    img: "https://acdn.mitiendanube.com/stores/052/236/products/whatsapp-image-2023-09-19-at-5-01-12-pm1-248fccb28fe71f24eb16951543146068-480-0.jpeg",
+    cantidad: 1,
+ },
+ {
+    id: 4,
+    nombre: 'Mocasines Danna',
+    precio: 57500,
+    img: "https://img.ltwebstatic.com/images3_pi/2023/05/05/16832170007a54c294fef441874eb4c6dbbd8edab9_thumbnail_720x.jpg",
+    cantidad: 1,
+ },
+];
